a9d646dacbae3dedef9a3c06826b06de2fd1da7c
KoriAntunez <kori.antunez@unmsm.edu.pe>
2022-08-25 17:01:31 -0500

    commit

--- src/main/java/King.java
+++ src/main/java/King.java
@@ -38,8 +38,22 @@ public class King
     @Override
     protected ArrayList<String> calculatePossibleMoves( ChessGameBoard board ){
         ArrayList<String> northEastMoves = calculateNorthEastMoves( board, 1 );
+        ArrayList<String> northWestMoves = calculateNorthWestMoves( board, 1 );
+        ArrayList<String> southEastMoves = calculateSouthEastMoves( board, 1 );
+        ArrayList<String> southWestMoves = calculateSouthWestMoves( board, 1 );
+        ArrayList<String> northMoves = calculateNorthMoves( board, 1 );
+        ArrayList<String> southMoves = calculateSouthMoves( board, 1 );
+        ArrayList<String> eastMoves = calculateEastMoves( board, 1 );
+        ArrayList<String> westMoves = calculateWestMoves( board, 1 );
         ArrayList<String> allMoves = new ArrayList<String>();
-
+        allMoves.addAll( northEastMoves );
+        allMoves.addAll( northWestMoves );
+        allMoves.addAll( southWestMoves );
+        allMoves.addAll( southEastMoves );
+        allMoves.addAll( northMoves );
+        allMoves.addAll( southMoves );
+        allMoves.addAll( westMoves );
+        allMoves.addAll( eastMoves );
         return allMoves;
     }
     /**
